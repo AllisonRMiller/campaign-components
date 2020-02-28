f54ea0938f1a5e4f719861cf9ca2b285ff312003
AllisonRMiller <60373159+AllisonRMiller@users.noreply.github.com>
2020-02-27 22:21:16 -0500

    Final commit
    Website complete!

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
-//open and close tab menu
-$('.nav-tabs-dropdown')
-    .on("click", "li:not('.active') a", function(event) {  $(this).closest('ul').removeClass("open");
-    })
-    .on("click", "li.active a", function(event) {        $(this).closest('ul').toggleClass("open");
-    });
 
-function on() {
-    document.getElementsByClassName("tab-pane").style.display = "block";
-}
-
-function off() {
-    document.getElementsByClassName("tab-pane").style.display = "none";
-}
+$('.overlay-image').affix({
+    offset:{
+        top: $('navbar').height() 
+    }
+})
